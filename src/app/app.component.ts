43ffda6df0b304ede6ae5c92424445ba93583294
asmitapaul22 <asmitapaul2000@gmail.com>
2020-08-25 11:18:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, Renderer2,HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice11';
+
+  constructor(private el:ElementRef, private renderer:Renderer2){
+  }
+@HostListener('click') onclick(){
+  alert("clicked!!!");
+}
+@HostListener('mouseenter') onmouseover(){
+  this.changebgcolor("red");
+  // console.log('move over');
+}
+@HostListener('mouseleave') onmouseleave(){
+  this.changebgcolor("blue")
+}
+changebgcolor(color:string){
+  this.renderer.setStyle(this.el.nativeElement,'color',color);
+}
+  
 }
